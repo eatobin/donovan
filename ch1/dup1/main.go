cb59a1a708c1d12115c0b971038b4f5e6fb8be38
Eric Tobin <eatobin@gmail.com>
2018-04-05 15:33:10 -0500

    Stdin srcipt

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -6,6 +6,16 @@
 
 // Dup1 prints the text of each line that appears more than
 // once in the standard input, preceded by its count.
+
+//Added by Eric:
+//[eric@linux-x2vq dup1](master)$ echo 'this is hello'   > /tmp/lines
+//[eric@linux-x2vq dup1](master)$ echo 'this is filter1' >> /tmp/lines
+//[eric@linux-x2vq dup1](master)$ echo 'this is filter2' >> /tmp/lines
+//[eric@linux-x2vq dup1](master)$ echo 'this is filter1' >> /tmp/lines
+//[eric@linux-x2vq dup1](master)$ echo 'this is hello'   >> /tmp/lines
+//[eric@linux-x2vq dup1](master)$ echo 'this is hello'   >> /tmp/lines
+//[eric@linux-x2vq dup1](master)$ cat /tmp/lines | go run main.go
+
 package main
 
 import (
