975a787e06299880717da7d3b51cbb17e93fc5df
Eric Tobin <eatobin@gmail.com>
2018-04-06 15:42:22 -0500

    and back...

--- ch1/fetchall/main.go
+++ ch1/fetchall/main.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.X
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 // See page 17.
